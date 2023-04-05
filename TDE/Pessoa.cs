7868039e339046b7797a1a21882c0dfff04b9db1
Maria das Graças <Maria das Graas@DESKTOP-SNEJ25I>
2023-04-05 19:39:07 -0300

    Adicionar arquivos de projeto.

---
+++ TDE/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDE
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Profissao { get; set; }
+        public string Cidade { get; set; }
+    }
+}
---
+++ TDE/Program.cs
@@ -0,0 +1,26 @@
+﻿using TDE;
+using Newtonsoft.Json;
+using System.IO;
+
+StreamReader leitor = new StreamReader(@"C:\temp\Arquivo.json");
+string conteudoArquivo = leitor.ReadToEnd();
+leitor.Close();
+
+List<Pessoa> pessoas = JsonConvert.DeserializeObject <List<Pessoa>>(conteudoArquivo);
+
+string json = JsonConvert.SerializeObject(pessoas);
+
+foreach (Pessoa pessoa in pessoas)
+{
+    Console.WriteLine("Nome: " + pessoa.Nome);
+    Console.WriteLine("Idade: " + pessoa.Idade);
+    Console.WriteLine("Profissao: " + pessoa.Profissao);
+    Console.WriteLine("Cidade: " + pessoa.Cidade);
+    Console.WriteLine("");
+}
+
+string caminhoArquivo = @"C:\temp\Arquivo_Criado.json";
+
+File.WriteAllText(caminhoArquivo, json);
+
+Console.ReadKey();
